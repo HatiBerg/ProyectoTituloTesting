8a52a0959f0779e04fc12ffc55513313f09087b5
Esteban Ponce <66049549+HatiBerg@users.noreply.github.com>
2024-04-21 23:53:39 -0400

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,8 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-
     bool cantJump;
 
     void Start()
@@ -18,10 +17,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         Movement();
     }
-    void FixedUpdate()
-    {
-        //gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0f;
-    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Floor")
@@ -34,14 +30,14 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (Input.GetKey("a"))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-500f * Time.deltaTime, 0));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000f * Time.deltaTime, 0));
             gameObject.GetComponent<Animator>().SetBool("moving", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
 
         if (Input.GetKey("d"))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(500f * Time.deltaTime, 0));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1000f * Time.deltaTime, 0));
             gameObject.GetComponent<Animator>().SetBool("moving", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
@@ -58,7 +54,7 @@ public class NewBehaviourScript : MonoBehaviour
         if (Input.GetKeyDown("w") && cantJump)
         {
             cantJump = false;
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 220f));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250f));
             gameObject.GetComponent<Animator>().SetTrigger("jumping");
         }
     }
