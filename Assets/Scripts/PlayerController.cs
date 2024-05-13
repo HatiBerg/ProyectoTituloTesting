fa1a6c60d70705ecc71b6ec5a6a50cdc656c00f8
Esteban Ponce <66049549+HatiBerg@users.noreply.github.com>
2024-05-13 17:33:32 -0400

    Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,61 +7,78 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     //private static GameObject player;
+    public Transform attackPoint;
+    public float attackRange = 1.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 20;
     bool cantJump;
-    bool inFall;
 
     void Start()
     {
         //player = GameObject.FindWithTag("Player");
     }
-    void FixedUpdate()
-    {
-        
-    }
 
     void Update()
     {
         Movement();
+        Combat();
     }
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "Floor")
-        {
             cantJump = true;
-        }
     }
 
     void Movement()
     {
-        if (Input.GetKey("a"))
+        if (Input.GetKey(KeyCode.A))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000f * Time.deltaTime, 0));
             gameObject.GetComponent<Animator>().SetBool("moving", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
 
-        if (Input.GetKey("d"))
+        if (Input.GetKey(KeyCode.D))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1000f * Time.deltaTime, 0));
             gameObject.GetComponent<Animator>().SetBool("moving", true);
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
-        if (!Input.GetKey("a") && !Input.GetKey("d"))
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
             gameObject.GetComponent<Animator>().SetBool("moving", false);
         }
 
-        if (Input.GetKeyDown("w") && cantJump)
+        if (Input.GetKeyDown(KeyCode.W) && cantJump)
         {
             cantJump = false;
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250f));
-            gameObject.GetComponent<Animator>().SetBool("jump", true);
+            gameObject.GetComponent<Animator>().SetTrigger("jump");
         }
+    }
 
-        if (cantJump)
+    void Combat()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            gameObject.GetComponent<Animator>().SetBool("jump", false);
+            gameObject.GetComponent<Animator>().SetTrigger("attack");
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("Enemigo golpeado:" + enemy.name);
+                enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            }
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
