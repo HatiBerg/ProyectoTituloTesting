b789d0138175c4fa398b5b77832a30fd40d47c4f
Esteban Ponce <66049549+HatiBerg@users.noreply.github.com>
2024-05-26 21:57:45 -0400

    Update DialogueUI.cs

--- Assets/DialogueSystem/Scripts/DialogueUI.cs
+++ Assets/DialogueSystem/Scripts/DialogueUI.cs
@@ -21,7 +21,7 @@ public class DialogueUI : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && dialogueContainer.activeInHierarchy)
         {
             UpdateText(1);
         }
